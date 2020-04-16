c35e6c32f77a32a26285bb0d33140a587f16377e
msm <msm@tailcall.net>
2020-04-16 02:51:14 +0200

    feat: implement a primitive readline replacement

--- terminal.cpp
+++ terminal.cpp
@@ -1,16 +1,34 @@
 #include "terminal.h"
 
+#include <cassert>
+
 namespace gib::terminal {
 
 void Terminal::enable_raw_mode() {
   tcgetattr(fd_, &saved_termios_);
-  struct termios new_termios {
-    saved_termios_
-  };
+  termios new_termios{saved_termios_};
   new_termios.c_lflag &= ~(ECHO | ICANON);
   tcsetattr(fd_, TCSAFLUSH, &new_termios);
 }
 
-void Terminal::disable_raw_mode() { tcsetattr(fd_, TCSAFLUSH, &saved_termios_); }
+uint8_t Terminal::read() {
+  uint8_t byte;
+  while (true) {
+    int result = ::read(fd_, &byte, 1);
+    if (result > 0) {
+      return byte;
+    }
+  }
+}
+
+void Terminal::write(uint8_t byte) { assert(::write(fd_, &byte, 1) == 1); }
+
+void Terminal::write(const std::string_view &text) {
+  assert(::write(fd_, text.data(), text.size()) == text.size());
+}
+
+void Terminal::disable_raw_mode() {
+  tcsetattr(fd_, TCSAFLUSH, &saved_termios_);
+}
 
 }  // namespace gib::terminal
